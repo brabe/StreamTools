a74ba761795afe5e94b91799c4f988378d0fa155
Brent Rabe <brabe9035@icloud.com>
2017-01-02 16:23:23 -0600

    Auto Resize Main Form (Form1)

--- StreamingSetupV2/Form1.Designer.cs
+++ StreamingSetupV2/Form1.Designer.cs
@@ -103,8 +103,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(798, 448);
+            this.ClientSize = new System.Drawing.Size(798, 424);
             this.Controls.Add(this.liveScoreInfo1);
             this.Controls.Add(this.halftimeCountdown1);
             this.Controls.Add(this.startingSoonCountdown1);
