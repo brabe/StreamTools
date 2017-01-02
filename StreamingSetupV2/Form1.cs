2f844920dca42aa3204b3ddb485f594487021028
Brent Rabe <brabe9035@icloud.com>
2017-01-02 17:40:03 -0600

    Simplify

--- StreamingSetupV2/Form1.cs
+++ StreamingSetupV2/Form1.cs
@@ -43,7 +43,7 @@ namespace StreamingSetupV2
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Close();
         }
     }
 }
